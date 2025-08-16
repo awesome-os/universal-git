445bfd1fba1edcc860c083c4741155ec270a7abd
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-16 10:06:24 +0200

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,4 @@ export type * from './internal-apis';
 export type * from './http/node/index';
 export type * from './http/managers/index';
 export type * from './http/models/index';
+export type * from './typedef';
