d50996dbd418772c3043d0fcd5c62ec502006921
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-16 11:35:37 +0200

    Update rollup.config.js

--- packages/browser-isomorphic-git/rollup.config.js
+++ packages/browser-isomorphic-git/rollup.config.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'node:fs/promises';
 import path from 'path';
 import { defineConfig } from 'rollup';
 import pkg from './package.json' with { type: 'json' };
@@ -16,7 +16,7 @@ const external = [
 
 // Modern modules
 const ecmaConfig = (input, output) => ({
-  input: `src/${input}`,
+  input: `${input}`,
   external: [...external],
   output: [
     {
@@ -58,29 +58,64 @@ const ecmaConfig = (input, output) => ({
 
 
 // TODO: handle the name GitHttp Correct make own module or something see _name usage
-const pkgify = (input, output, _name) => {
-  fs.mkdirSync(path.join(import.meta.dirname, output), { recursive: true })
-  fs.writeFileSync(
-    path.join(import.meta.dirname, output, 'package.json'),
-    JSON.stringify(
-      {
-        type: 'module',
-        main: 'index.js'
-      },
-      null,
-      2
-    )
-  )
-  return [
-    ecmaConfig(`${input}/index.js`, `${output}/index.js`),
-  ]
-}
+// const pkgify = (input, output, _name) => {
+//   fs.mkdirSync(path.join(import.meta.dirname, output), { recursive: true })
+//   fs.writeFileSync(
+//     path.join(import.meta.dirname, output, 'package.json'),
+//     JSON.stringify(
+//       {
+//         type: 'module',
+//         main: 'index.js'
+//       },
+//       null,
+//       2
+//     )
+//   )
+//   return [
+//     ecmaConfig(`${input}/index.js`, `${output}/index.js`),
+//   ]
+// }
 
-export default [
-  ecmaConfig('index.js', 'index.js'),
-  ecmaConfig('internal-apis.js', 'internal-apis.js'),
-  ecmaConfig('managers/index.js', 'managers/index.js'),
-  ecmaConfig('models/index.js', 'models/index.js'),
-  ...pkgify('http/node', 'http/node'),
-  ...pkgify('http/web', 'http/web', 'GitHttp'),
-]
+export default {
+  input: {
+    'index.js': 'src/index.js', 
+    'internal-apis.js': 'src/internal-apis.js', 
+    'managers.js': 'src/managers/index.js', 
+    'models.js': 'src/models/index.js', 
+    'http.js': 'src/http/web/index.js', 
+  },
+  external(id) {
+    // All modules that do not come from us are external by definition.
+    return id.includes('node_modules') && !id.includes('@isomorphic-git');
+  }, //[...external],
+  plugins: [
+    {
+      "name": "node-resolve",
+      "description": "uses node module resolve to link types correct and add extensions as needed and so on.",
+      async resolveId(source,importer,options) {
+        // This should resolve existing files with extension as also extension less imports via the package.json mappings.		
+        const resolution = await this.resolve(source, importer, options) || import.meta.resolve(source,importer);
+        const exists = await fs.stat()
+        // If it cannot be resolved or is external, just return it
+        // so that Rollup can display an error
+        if (resolution.external) return resolution;
+      }
+    }
+  ],
+  output: [
+    {
+      format: 'es',
+      dir: import.meta.dirname,
+    },
+  ],
+}
+  
+//   [
+//   ecmaConfig('src/index.js', 'index.js'),
+//   ecmaConfig('src/internal-apis.js', 'internal-apis.js'),
+//   ecmaConfig('src/managers/index.js', 'managers.js'),
+//   ecmaConfig('src/models/index.js', 'models.js'),
+//   ecmaConfig('src/http/web/index.js', 'http.js'),
+//   // ...pkgify('http/node', 'http/node'),
+//   //...pkgify('http/web', 'http/web', 'GitHttp'),
+// ]
