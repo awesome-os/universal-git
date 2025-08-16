9029669f8693b6ecc6652a891711c06bc277ec60
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-16 09:42:54 +0200

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,6 @@
 export type * from './http'
+      "isomorphic-git": ["./index.d.ts"],
+      "isomorphic-git/internal-apis": ["./src/internal-apis.d.ts"],
+      "isomorphic-git/http": ["./http/node/index.d.ts"],
+      "isomorphic-git/managers": ["./managers/index.d.ts"],
+      "isomorphic-git/models": ["./models/index.d.ts"]
