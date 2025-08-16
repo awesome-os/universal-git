75802bd2113db3e82a6ea3f8e1f65298b6a1bb80
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-16 16:38:49 +0200

    Update cli.cjs

--- cli.cjs
+++ cli.cjs
@@ -1,31 +1,25 @@
-#!/usr/bin/env node
-const fs = require('fs')
-
 const minimisted = require('minimisted')
+const git = require('./src/index.js')
 
-const git = require('.')
-
-const http = require('./http/node')
+// Implementations that get Injected
+const fs = require('node:fs')
+const http = require('./src/http.js')
 
 // This really isn't much of a CLI. It's mostly for testing.
 // But it's very versatile and works surprisingly well.
-
 minimisted(async function({ _: [command, ...args], ...opts }) {
   try {
-    const result = await git[command](
-      Object.assign(
-        {
-          fs,
-          http,
-          dir: '.',
-          onAuth: () => ({ username: opts.username, password: opts.password }),
-          headers: {
-            'User-Agent': `git/isogit-${git.version()}`,
-          },
-        },
-        opts
-      )
-    )
+    const result = await git[command]({
+      fs, http,
+      dir: '.',
+      onAuth() { 
+        return { username: opts.username, password: opts.password }; 
+      },
+      headers: {
+        'User-Agent': `git/isogit-${git.version()}`,
+      },
+      opts,
+    });
     if (result === undefined) return
     // detect streams
     if (typeof result.on === 'function') {
@@ -34,8 +28,8 @@ minimisted(async function({ _: [command, ...args], ...opts }) {
       console.log(JSON.stringify(result, null, 2))
     }
   } catch (err) {
-    process.stderr.write(err.message + '\n')
-    console.log(err)
+    // process.stderr.write(err.message + '\n')
+    console.error(err)
     process.exit(1)
   }
 })
