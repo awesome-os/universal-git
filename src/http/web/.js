4ca6e9eeae50d4a1a68c01460936cc4480d2414b
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-16 15:01:34 +0200

    Update and rename index.js to .js

--- src/http/web/.js
+++ src/http/web/.js
@@ -17,25 +17,18 @@ export async function request({
 }) {
   // streaming uploads aren't possible yet in the browser
   if (body) {
-    body = await collect(body)
+    body = await collect(body);
   }
-  const res = await fetch(url, { method, headers, body })
-  const iter =
-    res.body && res.body.getReader
-      ? fromStream(res.body)
-      : [new Uint8Array(await res.arrayBuffer())]
-  // convert Header object to ordinary JSON
-  headers = {}
-  for (const [key, value] of res.headers.entries()) {
-    headers[key] = value
-  }
+  
+  const res = await fetch(url, { method, headers, body });
+  
   return {
     url: res.url,
     method: res.method,
     statusCode: res.status,
     statusMessage: res.statusText,
-    body: iter,
-    headers: headers,
+    body: res.body,
+    headers: Object.fromEntries(res.headers.entries()),
   }
 }
 
