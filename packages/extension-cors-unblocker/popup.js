759cdf6648306e566811f20697c2a226d641f81c
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-23 08:51:02 +0200

    Update popup.js

--- packages/extension-cors-unblocker/popup.js
+++ packages/extension-cors-unblocker/popup.js
@@ -1,3 +1,7 @@
-document.getElementById('options-btn').addEventListener('click', () => {
+import { browserPromise } from './api/browser/browser.js';
+document.getElementById('options-btn').addEventListener('click', () => browserPromise.then((browser)=> {
   browser.runtime.openOptionsPage();
-});
+}));
+  
+  
+
