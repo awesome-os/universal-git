1ae7257668a0e22bccc3a57ee475ce085a6bb200
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-23 08:03:30 +0200

    Create detect-environment.js

---
+++ packages/extension-cors-unblocker/detect-environment.js
@@ -0,0 +1,4 @@
+// Note: Safari and FireFox do implement this compat shim.
+export const isExtension = globalThis.chrome && globalThis.chrome.runtime && globalThis.chrome.runtime.id;
+export const isFirefox = globalThis.browser && globalThis.browser.runtime && globalThis.browser.runtime.id;
+export const isChrome = isExtension && !isFirefox;
