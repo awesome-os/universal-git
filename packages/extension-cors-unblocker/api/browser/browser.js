ac28266dfabee19dbbe7b771b069a9dfb1220537
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-23 08:54:49 +0200

    Update browser.js

--- packages/extension-cors-unblocker/api/browser/browser.js
+++ packages/extension-cors-unblocker/api/browser/browser.js
@@ -1,3 +1,6 @@
+// In Chrome, it resolves to a browser.* namespace as an wrapped alias for chrome.*.
+// In Firefox/Safari, the browser.* namespace already exists, so it resolves to that.
+
 // Note: Safari and FireFox do implement this compat shim.
 export const isExtension = globalThis.chrome && globalThis.chrome.runtime && globalThis.chrome.runtime.id;
 export const isFirefox = globalThis.browser && globalThis.browser.runtime && globalThis.browser.runtime.id;
